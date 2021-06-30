d2ccdbde2dad8f454f9820a78e0c19c9dae8078e
Douglas Ribeiro <douglas100099@hotmail.com>
2021-06-30 16:16:42 -0300

    Add faltlist with sing's

--- src/components/SignsData/index.tsx
+++ src/components/SignsData/index.tsx
@@ -1,9 +1,21 @@
 import React, { memo } from 'react';
 import { View, Text } from 'react-native';
-import AquarioSVG from '../../../assets/svg/AquarioSVG';
 import { Colors } from '../../constants/styles';
 
 import { styles } from './styles';
+
+import AquarioSVG from '../../../assets/svg/AquarioSVG';
+import PeixesSVG from '../../../assets/svg/PeixesSVG';
+import AriesSVG from '../../../assets/svg/AriesSVG';
+import TouroSVG from '../../../assets/svg/TouroSVG';
+import GemeosSVG from '../../../assets/svg/GemeosSVG';
+import CancerSVG from '../../../assets/svg/CancerSVG';
+import LeaoSVG from '../../../assets/svg/LeaoSVG';
+import VirgemSVG from '../../../assets/svg/VirgemSVG';
+import LibraSVG from '../../../assets/svg/LibraSVG';
+import EscorpiaoSVG from '../../../assets/svg/EscorpiaoSVG';
+import SagitarioSVG from '../../../assets/svg/SagitarioSVG';
+import CapricornioSVG from '../../../assets/svg/CapricornioSVG';
 
 interface SignsDataProps {
     data: HoroscopesArrayResultProps;
@@ -11,43 +23,92 @@ interface SignsDataProps {
 
 const SignsData = ({ data }: SignsDataProps) => {
 
-    const renderColor = (sign:string) => {
-        switch(sign){
-            case "Aquário" :
-                return "#fed5d1";
-            case "Peixes" :
-                return "#d0e8ea";
-            case "Áries" :
-                return "#f9fdcb";
-            case "Touro" :
-                return "#dbec85";
-            case "Gêmeos" :
-                return "#c5e4d8";
-            case "Câncer" :
-                return "#edc8e9";
-            case "Leão" :
-                return "#c5cdf4";
-            case "Virgem" :
-                return "#ffefcb";
-            case "Libra" :
-                return "#f5b6ab";
-            case "Escorpião" :
-                return "#e5cbb9";
-            case "Sagitário" :
-                return "#c5e7ab";
-            case "Capricórnio" :
-                return "#c8deed";
-            default :
-                return Colors.Primary;
-        }   
+    const renderSign = (sign: string) => {
+        switch (sign) {
+            case "Aquário":
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: "#fed5d1" }]}>
+                        <AquarioSVG />
+                    </View>
+                );
+            case "Peixes":
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: "#d0e8ea" }]}>
+                        <PeixesSVG />
+                    </View>
+                );
+            case "Áries":
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: "#f9fdcb" }]}>
+                        <AriesSVG />
+                    </View>
+                );
+            case "Touro":
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: "#dbec85" }]}>
+                        <TouroSVG />
+                    </View>
+                );
+            case "Gêmeos":
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: "#c5e4d8" }]}>
+                        <GemeosSVG />
+                    </View>
+                );
+            case "Câncer":
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: "#edc8e9" }]}>
+                        <CancerSVG />
+                    </View>
+                );
+            case "Leão":
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: "#c5cdf4" }]}>
+                        <LeaoSVG />
+                    </View>
+                );
+            case "Virgem":
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: "#ffefcb" }]}>
+                        <VirgemSVG />
+                    </View>
+                );
+            case "Libra":
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: "#f5b6ab" }]}>
+                        <LibraSVG />
+                    </View>
+                );
+            case "Escorpião":
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: "#e5cbb9" }]}>
+                        <EscorpiaoSVG />
+                    </View>
+                );
+            case "Sagitário":
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: "#c5e7ab" }]}>
+                        <SagitarioSVG />
+                    </View>
+                );
+            case "Capricórnio":
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: "#c8deed" }]}>
+                        <CapricornioSVG />
+                    </View>
+                );
+            default:
+                return (
+                    <View style={[styles.CardImage, { backgroundColor: Colors.Primary }]}>
+                        <AquarioSVG />
+                    </View>
+                );
+        }
     };
 
     return (
         <View style={styles.CardView}>
-            {/* {renderSign(data.sign)} */}
-            <View style={[styles.CardImage, { backgroundColor: renderColor(data?.sign) }]}>
-                <AquarioSVG />
-            </View>
+            {renderSign(data.sign)}
             <View style={styles.ViewTextSign}>
                 <Text style={styles.TxtSign}>{data?.sign}</Text>
             </View>
